330ec78013eb355c7f4614195921e2655d137caf
Victor Andre de Paula e Silva <victorandre.ps@gmail.com>
2022-05-02 14:42:23 +1000

    APP  Ask me Anything
    APP performed during the iOS & Swift - The Complete iOS App Development Bootcamp

--- Ask me anything/ViewController.swift
+++ Ask me anything/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var ballImage: UIImageView!
+    
+    let ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askButtonPressesd(_ sender: UIButton) {
+        
+        ballImage.image = ballArray.randomElement()
+        
+    }
+    
 }
 
